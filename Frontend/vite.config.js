3b93ec271fc11a6edf7e878cef90ad6e1e50bc72
Koosell <nasalzamaizzar29@gmail.com>
2025-06-22 00:45:30 +0700

    Initial project setup for user pages

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Konfigurasi base path untuk deploy ke sub-folder di GitHub Pages
-  base: '/Identitas/', 
-  plugins: [vue()],
-  
+  // Diubah untuk deploy ke alamat utama (root)
+  base: '/', 
+
   plugins: [
     vue(),
     vueDevTools(),
